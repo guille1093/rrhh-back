f9781c10c07b8a907611daf9c2db991725bb40cf
Guillermo Quintana <guillermo.quintana@shakeagain.net>
2025-09-20 12:12:46 -0300

    del dist

--- dist/src/auth/auth.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Auth = exports.PERMISSIONS_KEY = void 0;
-const common_1 = require("@nestjs/common");
-exports.PERMISSIONS_KEY = 'permissions';
-const Auth = (...permissions) => (0, common_1.SetMetadata)(exports.PERMISSIONS_KEY, permissions);
-exports.Auth = Auth;
-//# sourceMappingURL=auth.decorator.js.map
--- dist/src/positions/dto/create-position.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreatePositionDto {
-    name: string;
-    departmentId: number;
-}
